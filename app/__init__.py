601c8e5d7cfd1592d748b6b8f5a751eb890c8d59
billowbashir <billowbashir@gmail.com>
2018-09-07 19:03:49 +0300

    add email column to user class

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,13 @@ from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 bootstrap=Bootstrap()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app= Flask(__name__)
@@ -18,6 +22,8 @@ def create_app(config_name):
     # app.config['SECRET_KEY'] ='1234'
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
+
 
 
 
