45742e9a7555ab51403b066ddd97d24459bcf23a
billowbashir <billowbashir@gmail.com>
2018-09-10 12:12:47 +0300

    successfully add pitches to database

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,8 @@
-from flask import render_template, url_for
+from flask import render_template, url_for,redirect
 from . import main
 from . forms import PitchForm
-
+from ..models import Pitch, Comment, User
+from .. import db
 
 @main.route('/')
 def index():
@@ -11,4 +12,9 @@ def index():
 @main.route('/pitch',methods=['GET','POST'])
 def pitch():
     pitch_form=PitchForm()
+    if pitch_form.validate_on_submit():
+        pitch=Pitch(category=pitch_form.category.data,pitch=pitch_form.pitch.data)
+        db.session.add(pitch)
+        db.session.commit()
+        return redirect(url_for('main.index'))
     return render_template('pitch.html',pitch_form=pitch_form)
