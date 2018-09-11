610227c42f9c49fdb910f42a0fb5cf93eada68e1
billowbashir <billowbashir@gmail.com>
2018-09-11 19:39:13 +0300

    add MAIL_USE_SSL = True, in config

--- config.py
+++ config.py
@@ -26,6 +26,7 @@ class ProdConfig(Config):
     MAIL_SERVER='smtp.gmail.com'
     MAIL_PORT=587
     MAIL_USE_TLS=True
+    MAIL_USE_SSL = True
     MAIL_USERNAME=os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD=os.environ.get("MAIL_PASSWORD")
 
