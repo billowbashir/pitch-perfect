ab1492b87ca055f8bb4ccdd4143e55a6cca84f2a
billowbashir <billowbashir@gmail.com>
2018-09-11 08:45:13 +0300

    debugg email

--- config.py
+++ config.py
@@ -6,15 +6,7 @@ class Config:
     class config
     '''
 
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    
 
-     # email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
     '''
@@ -24,6 +16,15 @@ class ProdConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+     # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 class DevConfig(Config):
