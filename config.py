ea86323446458c26bf25234d37fca3d3d9bb381b
billowbashir <billowbashir@gmail.com>
2018-09-11 19:29:08 +0300

    alter configs

--- config.py
+++ config.py
@@ -25,6 +25,7 @@ class ProdConfig(Config):
 
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
+    MAIL_SUPPRESS_SEND = False
     MAIL_USE_TLS = False
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
