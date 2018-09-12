70e56a5be2ab93813678683ff13a8819d4c9085b
billowbashir <billowbashir@gmail.com>
2018-09-12 19:14:52 +0300

    debugg

--- config.py
+++ config.py
@@ -18,17 +18,19 @@ class ProdConfig(Config):
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
-
-
-
-
-
     MAIL_SERVER='smtp.gmail.com'
     MAIL_PORT=587
     MAIL_USE_TLS=True
     MAIL_USE_SSL = True
     MAIL_USERNAME=os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD=os.environ.get("MAIL_PASSWORD")
+
+
+
+
+
+
+
 
 
 
