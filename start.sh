f53f563b4fb9934a69dd9835c8fd319239636db4
billowbashir <billowbashir@gmail.com>
2018-09-14 12:14:30 +0300

    fix start.sh

--- start.sh
+++
@@ -1,5 +0,0 @@
-export SECRET_KEY='12345'
-export MAIL_USERNAME='billowbashir@gmail.com'
-export MAIL_PASSWORD='bashiir4365'
-export DATABASE_URL='postgresql+psycopg2://bashir:bashiir@localhost/pitch_perfect'
-python3.6 manage.py server
